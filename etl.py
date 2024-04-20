8f715acbf33c41fe35587060b363d85e8b484acd
Arafat Nayeem <arafatnayeemxp@gmail.com>
2024-04-20 18:05:02 +0600

    Adding load_to_csv func

--- etl.py
+++ etl.py
@@ -60,5 +60,5 @@ def log_progress(message):
     timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
     now = datetime.now() # get current timestamp 
     timestamp = now.strftime(timestamp_format) 
-    with open(log_file,"a") as f: 
+    with open('log.csv',"a") as f: 
         f.write(timestamp + ',' + message + '\n') 
